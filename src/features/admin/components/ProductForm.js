f915cb80b5cd37f7b8f1143945cfaa1c53727585
HarshJoshiIIITV <joshih1998@gmail.com>
2023-08-16 09:58:55 +0530

    fixing errors in productForm page

--- src/features/admin/components/ProductForm.js
+++ src/features/admin/components/ProductForm.js
@@ -99,7 +99,7 @@ function ProductForm() {
           </h2>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          {selectedProduct.deleted && <h2 className="text-red-500 sm:col-span-6">This product is deleted</h2>}
+          {selectedProduct && selectedProduct.deleted && <h2 className="text-red-500 sm:col-span-6">This product is deleted</h2>}
 
             <div className="sm:col-span-6">
               <label
@@ -448,7 +448,7 @@ function ProductForm() {
       </div>
     </form>
     <Modal
-        title={`Delete ${selectedProduct.title}`}
+        title={`Delete ${selectedProduct && selectedProduct.title}`}
         message="Are you sure you want to delete this Product ?"
         dangerOption="Delete"
         cancelOption="Cancel"
